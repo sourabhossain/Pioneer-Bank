3f430995a7c878d7a2bec474a94b4aa587ef10f2
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-01 23:02:49 +0600

    login js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#login").addEventListener("click", function(event) {
+    const loginArea = document.querySelector("#login-area");
+    loginArea.style.display = "none";
+});
