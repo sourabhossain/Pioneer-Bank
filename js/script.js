cac591ada3b857076bd7810f51f7b46c7b5d8b33
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-02 08:00:19 +0600

    add deposit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
+// login button event handler
 document.querySelector("#login").addEventListener("click", function(event) {
     const loginArea = document.querySelector("#login-area");
     loginArea.style.display = "none";
 
     const transationArea = document.querySelector("#transation-area");
     transationArea.style.display = "block";
+});
+
+// deposit button event handler
+document.querySelector("#addDeposit").addEventListener("click", function(event) {
+    const depositAmount = document.querySelector("#depositAmount");
+    const depositNumber = parseFloat(depositAmount.value);
+    
+    const currentDeposit = document.querySelector("#currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    document.querySelector("#currentDeposit").innerText = totalDeposit;
+    document.querySelector("#depositAmount").value = "";
 });
