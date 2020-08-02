ae1f192753b584a3b2ee3b664fbac50bf3f9e189
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-02 10:01:59 +0600

    add get input number function

--- js/script.js
+++ js/script.js
@@ -8,15 +8,27 @@ document.querySelector("#login").addEventListener("click", function(event) {
 });
 
 // deposit button event handler
-document.querySelector("#addDeposit").addEventListener("click", function(event) {
-    const depositAmount = document.querySelector("#depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
+document.querySelector("#addDeposit").addEventListener("click", function() {
+    const depositNumber =  getInputNumber("#depositAmount")
     
     updateSpanText("#currentDeposit", depositNumber);
     updateSpanText("#currentBanlance", depositNumber);
 
     document.querySelector("#depositAmount").value = "";
 });
+
+// withdraw button event handler
+document.querySelector("#addWithdraw").addEventListener("click", function() {
+    const withdrawNumber = getInputNumber("#withdrawAmount");
+     
+    document.querySelector("#withdrawAmount").value = "";
+});
+
+function getInputNumber(id) {
+    const amount = document.querySelector(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
 
 function updateSpanText(id, depositNumber) {
     const current = document.querySelector(id).innerText;
